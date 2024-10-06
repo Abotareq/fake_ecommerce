c170a7d5440679ba8cc927b061adb2953de48cc4
Abotareq <7modetareq@gmail.com>
2024-10-06 20:52:42 +0300

    first_upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-9xl text-yellow-400 font-extrabold ">
+        {" "}
+      </h1>
+    </>
+  );
+}
+
+export default App;
